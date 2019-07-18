25fe32b01ff6955422e376f44005c62049a26cd0
Stanislav Hrytcyshyn <grucushun90@gmail.com>
2019-07-18 20:35:25 +0300

    First implementation of NN

--- nn_from_scratch.py
+++ nn_from_scratch.py
@@ -1,23 +1,151 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
-class NN(object):
-    def __init__(self, input_data, epoch_count):
-        pass
+class Activation(object):
+    @classmethod
+    def sigmoid(cls, x):
+        return 1 / 1 + math.e ** (-x)
 
     @classmethod
-    def activation_func(x):
-        '''
-            x - it's the output of every neuron in layer
-            output_of_neuron = input_of_neuron * weight_of_neuron
-        '''
-        # there are 2 activation functions
-        # you can choose wich one to use by your own
-        def sigmoid(value):
-            return 1 / 1 + np.exp(-value)
+    def sigmoid_derivative(cls, x):
+        return Activation.sigmoid(x) * (1 - Activation.sigmoid(x))
 
-        def htan(value):
-            # hyperbolic tangent function
-            return np.exp(2 * value) - 1 / np.exp(2 * value) + 1
+    @classmethod
+    def tanh(cls, x):
+        return np.tanh(x)
 
-        # in current NN we are using sigmoid activation function
-        return sigmoid(float(x))
+    @classmethod
+    def tanh_prime(cls, x):
+        return 1 - np.tanh(x) ** 2
+
+
+
+
+# loss function and its derivative
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true-y_pred, 2))
+
+def mse_prime(y_true, y_pred):
+    return 2*(y_pred-y_true)/y_true.size
+
+
+
+
+
+class InitialLayer(object):
+    def __init__(self, input_size, output_size):
+        # inp size - size of inputs neurons , output size - size of output neurons
+        self.weights = np.random.randn(input_size, output_size) / np.sqrt(input_size)
+        self.bias    = np.random.randn(1, output_size) - 0.5 # 0.5 - floating value
+
+    def forward_propagation(self, input_data):
+        self.input  = input_data
+        self.output = np.dot(self.input, self.weights) + self.bias
+
+        return self.output
+
+    def back_propagation(self, output_error, learning_rate):
+        input_error   = np.dot(output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error)
+        # dBias = output_error
+
+        # update parameters
+        self.weights -= learning_rate * weights_error
+        self.bias    -= learning_rate * output_error
+
+        return input_error
+
+
+class ActivationLayer(object):
+    def __init__(self, activation_func, prime_activation_func):
+        self.activation       = activation_func
+        self.prime_activation = prime_activation_func
+
+    def forward_propagation(self, input_data):
+        self.input  = input_data
+        self.output = self.activation(self.input)
+
+        return self.output
+
+    def back_propagation(self, output_error, learning_rate):
+        return self.prime_activation(self.input) * output_error
+
+glob_err = []
+
+class NN (object):
+    def __init__(self):
+        self.layers     = []
+        self.cost       = None
+        self.cost_prime = None
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+    def set_cost_functions(self, cost_func, cost_prime_func):
+        self.cost       = cost_func
+        self.cost_prime = cost_prime_func
+
+    def predict_values(self, input_data):
+        sample_size = len(input_data)
+        result = []
+        for i in range(sample_size):
+            output = input_data[i]
+            for layer in self.layers:
+                output = layer.forward_propagation(output)
+            result.append(output)
+        return result
+
+    def train(self, input_train, output_train, epochs, learning_rate):
+        # sample dimension first
+        sample_size = len(x_train)
+
+        # training loop
+        for i in range(epochs):
+            err = 0
+            for j in range(sample_size):
+                # forward propagation
+                output = x_train[j]
+                for layer in self.layers:
+                    output = layer.forward_propagation(output)
+
+                # compute loss (for display purpose only)
+                err += self.cost(y_train[j], output)
+
+                # backward propagation
+                error = self.cost_prime(y_train[j], output)
+                for layer in reversed(self.layers):
+                    error = layer.back_propagation(error, learning_rate)
+
+            # calculate average error on all samples
+            err /= sample_size
+            glob_err.append(err)
+            print('epoch %d/%d   error=%f' % (i+1, epochs, err))
+
+
+# year / distance / motor / kpp
+x_train = np.array([[[2.05, 0.330]], [[2.19, 0.5]], [[2.14, 0.65]], [[2.09, 0.137]]])
+y_train = np.array([[[0.09]], [[0.48]], [[0.15]], [[0.1150]]])
+
+net = NN()
+net.add_layer(InitialLayer(2, 2))
+net.add_layer(ActivationLayer(Activation.tanh, Activation.tanh_prime))
+net.add_layer(InitialLayer(2, 1))
+net.add_layer(ActivationLayer(Activation.tanh, Activation.tanh_prime))
+# train
+net.set_cost_functions(mse, mse_prime)
+net.train(x_train, y_train, epochs=1000, learning_rate=0.1)
+# test
+out = net.predict_values(x_train)
+print(out)
+
+trys1 = np.array([[2.13, 0.81]])
+trys2 = np.array([[2.03, 0.275]])
+
+myt1 = net.predict_values(trys1)
+myt2 = net.predict_values(trys2)
+print(myt1)
+print(myt2)
+
+plt.plot(range(1000), glob_err)
+plt.show()
