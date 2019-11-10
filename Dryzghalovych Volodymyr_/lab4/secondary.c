2e7e80ff9a01817efb521da8abd2c39cd3fa3c40
RakVojak <tottiky@gmail.com>
2019-11-10 20:55:02 +0200

    Add files via upload

---
+++ Dryzghalovych Volodymyr_/lab4/secondary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void hell()
+{
+printf("Hello World!\n");
+}
+
+void calc(double a, double b)
+
+{
+printf("a+b=%le \n",a+b);
+printf("a-b=%le \n",a-b);
+printf("a*b=%le \n",a*b);
+printf("a/b=%le \n",a/b);
+}
+
